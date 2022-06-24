19179bb09d6304ed583f76e06decfda4d4483720
Adriaan Boshoff <60996737+abmakes@users.noreply.github.com>
2022-06-24 15:26:23 +0700

    CSP added to config

--- next.config.js
+++ next.config.js
@@ -4,3 +4,33 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self' *fonts.googleapis.com;
+  font-src 'self';  
+`
+
+const securityHeaders = []
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+      {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+      },
+      {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+      }
+
+    ]
+  },
+}
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
-          <meta http-equiv="Content-Security-Policy" content="script-src 'none'" />
       </Head>
       <body>
         <Main />
